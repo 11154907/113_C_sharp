4aee66866696c02ea3f2224890e06f8cba141451
a113221027 <a113221027@mail.shu.edu.tw>
2024-12-03 18:02:19 +0800

    113-12-03

---
+++ 113-12-03/Tutorial 5-5-1/Friend File/Friend File/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Friend_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeNameButton_Click(object sender, EventArgs e)
+        {
+            // 這裡是寫入名字的按鈕點擊事件處理程式
+            try
+            {
+                StreamWriter outputFile; //StreamWriter 物件
+                if (saveFile.ShowDialog() == DialogResult.OK)
+                {
+                    // outputFile = File.AppendText(@"C:\Users\jerry\Desktop\Friends.txt"); //開啟檔案
+                    // outputFile = File.AppendText(@"C:\Users\shu\Desktop\113-12-03\113_C_sharp\113-12-03\Tutorial 5-5-1\Friend File\data\Friends.txt");
+                    outputFile = File.AppendText(saveFile.FileName);
+                    outputFile.WriteLine(nameTextBox.Text);  //寫入名字
+                    outputFile.Close(); //關閉檔案
+                    MessageBox.Show("名字以寫入檔案"); //顯示訊息
+                    outputFile.Close ();    //關閉檔案
+                }
+
+                //outputFile = File.AppendText(
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);    
+            }
+
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
---
+++ 113-12-03/Tutorial 5-7/Total Sales/Total Sales/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Total_Sales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            // 計算按鈕點擊事件處理
+            try
+            {
+                //宣告變數
+                decimal total = 0m;
+                decimal sales;
+               //tring input;
+
+                //建立 StreamReader 物件
+                StreamReader inputFile;
+
+                //開啟檔案
+                inputFile = File.OpenText("Sale.txt");
+
+                //讀取檔案內容
+                while (inputFile.EndOfStream)
+                {
+                    //讀取一行
+                    //inout = imputFile.PeadLine();
+
+                    //將讀取資料轉換為 decimal
+                    if (decimal.TryParse(inputFile.ReadLine(), out sales))
+                    {
+                        //加總
+                        total += sales;
+                    }
+                    else
+                    {
+                        //顯示錯誤訊息
+                        MessageBox.Show("Invalid input");
+                    }
+                }
+
+                //關閉檔案
+                inputFile.Close();
+
+                //顯示銷售總額
+                totalLabel.Text = total.ToString("c");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
